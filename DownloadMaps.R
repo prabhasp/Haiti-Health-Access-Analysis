94195a80384ac3cbcac8fcf4a59b59173a4db268
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-10-25 17:49:44 -0400

    correct filename

--- DownloadMaps.R
+++ DownloadMaps.R
@@ -1,4 +1,4 @@
 require(OpenStreetMap)
-map <- openmap(c(20.088, -74.479), c(18.021, -71.613), minNumTiles=9, type="osm")
+map <- openmap(c(20.088, -74.479), c(18.021, -71.613), minNumTiles=9, type="osm-bbike")
 mapprojected <- openproj(map, CRS('+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0'))
-saveRDS(mapprojected, 'data/HaitiBaseMap_utm.rds')
+saveRDS(mapprojected, 'data/HaitiBaseMapPlain_utm.rds')
